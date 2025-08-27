3d9c2e4fcf6d48e9d4011957f1bdab4bbc462fdf
Luiz8734 <luizmoraissantos17@gmail.com>
2025-08-27 20:57:07 -0300

    DADOS INICIAIS ADICIONADOS

---
+++ app.js
@@ -0,0 +1,58 @@
+// Dados iniciais das jogadoras
+let initialPlayers = [
+    {
+        "id": 1,
+        "nome": "Andressa Alves",
+        "posicao": "Meio-campo",
+        "clube": "Corinthians",
+        "foto": "https://imgs.search.brave.com/gbuCbhClpF4xWq4i1MSuo1DxP_sVUxQUcUTYc_80G-Q/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bWV1dGltYW8uY29t/LmJyL191cGxvYWQv/bm90aWNpYS8yMDI0/LzEyLzEzL28tY29y/aW50aGlhbnMtYW51/bmNpb3UtYW5kcmVz/c2EtYWx2ZXMtcGFy/YS0yMDI1LWp0OTQx/dy5qcGc",
+        "gols": 15,
+        "assistencias": 10,
+        "jogos": 28,
+        "favorita": false
+    },
+    {
+        "id": 2,
+        "nome": "Dayana Rodríguez",
+        "posicao": "Meio-campo",
+        "clube": "Corinthians",
+        "foto": "https://cdn.meutimao.com.br/_upload/jogador/dayana-lisset-rodriguez-leon-no-corinthians_a.jpg",
+        "gols": 5,
+        "assistencias": 12,
+        "jogos": 30,
+        "favorita": false
+    },
+    {
+        "id": 3,
+        "nome": "Mariza",
+        "posicao": "Zagueira",
+        "clube": "Corinthians",
+        "foto": "https://imgs.search.brave.com/JttraY5CnG_Ut7d7iWUQSzuIO2zRn4D-5GzJf5bEB7U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aW1nLnplcm96ZXJv/LnB0L2ltZy9qb2dh/ZG9yZXMvbmV3LzY0/LzA1LzUyNjQwNV9t/YXJpemFfMjAyNTA3/MjMxOTQwMDAucG5n",
+        "gols": 2,
+        "assistencias": 1,
+        "jogos": 32,
+        "favorita": false
+    },
+    {
+        "id": 4,
+        "nome": "Thaís Regina",
+        "posicao": "Zagueira",
+        "clube": "Corinthians",
+        "foto": "https://imgs.search.brave.com/2w-BPaUcsE5nQ5QXCeb4NUXbj7-t-L53_ra5NmmLnWU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuY29yaW50aGlh/bnMuY29tLmJyL3Vw/bG9hZHMvMTc1MDk0/Njk3MWExMWRjMzNk/OTA4OGI4MmY4MGRj/ZGE5ODQxNjM2MjI5/LnBuZw",
+        "gols": 1,
+        "assistencias": 2,
+        "jogos": 25,
+        "favorita": false
+    },
+    {
+        "id": 5,
+        "nome": "Letícia Teles",
+        "posicao": "Zagueira",
+        "clube": "Corinthians",
+        "foto": "https://imgs.search.brave.com/ZEylpJ77IoZs8ZjUEbXTtPE0p9gW6WU6czGvdY0vMYA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuY29yaW50aGlh/bnMuY29tLmJyL3Vw/bG9hZHMvMTc1MDk0/NjM2OWNkY2IyZjVj/N2IwNzExNDM1Mjll/ZjdmMjcwNWRmYmM0/LnBuZw",
+        "gols": 0,
+        "assistencias": 0,
+        "jogos": 18,
+        "favorita": false
+    }
+];
